c91668fe02e535bc3198786132ee0e84bd4c6a35
MailsonSilva <mailsonfcsilva@gmail.com>
2022-02-18 14:55:13 -0300

    atualizando

--- Chapter II/gofinances/src/screens/Dashboard/index.tsx
+++ Chapter II/gofinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
-import {Container, Title} from './styles';
+import {
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+} from './styles';
 
 export default function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserInfo>
+                    <Photo 
+                        source={{ uri: 'https://avatars.githubusercontent.com/u/18700076?v=4.png'}}
+                    />
+                    <User>
+                        <UserGreeting>Olá</UserGreeting>
+                        <UserName>Mailson</UserName>
+                    </User>
+                </UserInfo>
+               
+            </Header>            
         </Container>
     )
 }
