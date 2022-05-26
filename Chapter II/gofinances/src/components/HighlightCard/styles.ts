22abd78e4805091c02e1ed6758d3555d5632b42e
MailsonSilva <mailsonfcsilva@gmail.com>
2022-05-26 16:59:20 -0300

    estilizando container_card

--- Chapter II/gofinances/src/components/HighlightCard/styles.ts
+++ Chapter II/gofinances/src/components/HighlightCard/styles.ts
@@ -1,12 +1,22 @@
 import styled from "styled-components/native";
 import { Feather } from '@expo/vector-icons';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-
+    background-color: ${({ theme}) => theme.colors.shape};
+    height: ${RFPercentage(20)}px;
+    width: ${RFPercentage(34)}px;
+    position: relative;
+    top: ${RFPercentage(-10)}px;
+    border-radius: 6px;
+    padding: 14px;
 `;
 
 export const Header = styled.View`
-
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: ${RFValue(20)}px;;
 `;
 
 export const Title = styled.Text`
