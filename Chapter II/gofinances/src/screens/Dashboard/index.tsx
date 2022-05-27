b4afe3521fcd24169ad7545225f5534aab49ee8b
MailsonfcSilva <mailsonfcsilva@gmail.com>
2022-05-27 20:47:24 -0300

    acessando propriedades do card

--- Chapter II/gofinances/src/screens/Dashboard/index.tsx
+++ Chapter II/gofinances/src/screens/Dashboard/index.tsx
@@ -32,9 +32,24 @@ export default function Dashboard() {
             </Header>     
 
             <HighlightCards>       
-                <HighlightCard/>       
-                <HighlightCard/>       
-                <HighlightCard/>       
+                <HighlightCard
+                    type='up'
+                    title='Entradas'
+                    amount='R$ 17.400,00'
+                    lastTransaction='Última entrada dia 13 de novembro'
+                />       
+                <HighlightCard
+                    type='down'
+                    title='Saídas'
+                    amount='R$ 1.259,00'
+                    lastTransaction='Última saída dia 9 de novembro'
+                />
+                <HighlightCard
+                    type='total'
+                    title='Total'
+                    amount='R$ 16.141,00'
+                    lastTransaction='01 à 16 de novembro'
+                />     
             </HighlightCards> 
 
         </Container>
